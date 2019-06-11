bf43f7f9c7cae18a46b507d3422e628ad0bfe2ce
Your Name <dwproduction5@DWproduction5.local>
2019-06-11 10:01:21 -0500

    Event Listeners for setTime, and setFocus added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const time = document.getElementById("time"),
   name = document.getElementById("name"),
   focus = document.getElementById("focus");
 
+//Options
+const showAmPm = true;
+
 //Show Time
 function showTime() {
   let today = new Date(),
@@ -24,7 +27,7 @@ function showTime() {
 
   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
     sec
-  )}`;
+  )} ${showAmPm ? amPm : ""}`;
 
   setTimeout(showTime, 1000);
 }
@@ -57,6 +60,20 @@ function setBgGreet() {
   }
 }
 
+//Set Name
+
+function setName(e) {
+  if (e.type === "keypress") {
+    //Make Sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+
 //Get Name
 
 function getName() {
@@ -75,6 +92,25 @@ function getFocus() {
     focus.textContent = localStorage.getItem("focus");
   }
 }
+
+//Set Focus
+
+function setFocus(e) {
+  if ((e.type = "keypress")) {
+    //Make sure enter is pressed
+    if (e.which === 13 || e.keyCode === 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    } else {
+      localStorage.setItem("focus", e.target.innerText);
+    }
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
 
 //Run
 showTime();
